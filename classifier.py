b74fbe516c430b2862ea1b8c5928efee2410a665
DPigeon <D_Pig@encs.concordia.ca>
2020-03-17 16:08:58 -0400

    #6: Reading the training set

--- classifier.py
+++ classifier.py
@@ -6,12 +6,19 @@ inputPath = "input/input.txt"
 def main():
     the_input = input_parser.InputParser(inputPath)
 
+    # Getting hyper=parameters
     v = the_input.get_vocabulary()
     n = the_input.get_n_gram_size()
     d = the_input.get_smoothing_value()
     training_file = the_input.get_training_file()
     testing_file = the_input.get_testing_file()
 
-    print(v, n, d, training_file, testing_file)
+    # Reading the training set
+    the_input.read_set_file("training")
+
+    tweet_ids = the_input.get_tweet_training_ids()
+    tweet_usernames = the_input.get_tweet_training_usernames()
+    tweet_languages = the_input.get_tweet_training_languages()
+    tweet_messages = the_input.get_tweet_training_messages()
 
 main()
--- input_parser.py
+++ input_parser.py
@@ -6,6 +6,18 @@ class InputParser:
     smoothing_value = 0 # Smoothing value between 0 and 1
     training_file = "" # File used for training
     testing_file = "" # File used for testing
+
+    # Tweets for training (id, username, language, post)
+    tweet_training_ids = []
+    tweet_training_usernames = []
+    tweet_training_languages = []
+    tweet_training_messages = []
+
+    # Tweets for testing (id, username, language, post)
+    tweet_testing_ids = []
+    tweet_testing_usernames = []
+    tweet_testing_languages = []
+    tweet_testing_messages = []
     
     def __init__(self, file_path):
         with open(file_path) as file:
@@ -17,6 +29,26 @@ class InputParser:
                 self.smoothing_value = number[2]
                 self.training_file = number[3]
                 self.testing_file = number[4]
+    
+    def read_set_file(self, mode): # Two read modes: Mode 1: training, Mode 2: test
+        if mode == "training":
+            read_file = "input/" + self.training_file + ".txt"
+            with open(read_file, encoding="utf8") as file:
+                for line in file:
+                    tweet = line.split("\t") # Each line is a tweet
+                    self.tweet_training_ids.append(tweet[0])
+                    self.tweet_training_usernames.append(tweet[1])
+                    self.tweet_training_languages.append(tweet[2])
+                    self.tweet_training_messages.append(tweet[3])
+        else:
+            read_file = "input/" + self.testing_file + ".txt"
+            with open(read_file, encoding="utf8") as file:
+                for line in file:
+                    tweet = line.split("\t")
+                    self.tweet_testing_ids.append(tweet[0])
+                    self.tweet_testing_usernames.append(tweet[1])
+                    self.tweet_testing_languages.append(tweet[2])
+                    self.tweet_testing_messages.append(tweet[3])
     
     def get_vocabulary(self):
         return self.vocabulary
@@ -32,3 +64,27 @@ class InputParser:
 
     def get_testing_file(self):
         return self.testing_file
+        
+    def get_tweet_training_ids(self):
+        return self.tweet_training_ids
+
+    def get_tweet_training_usernames(self):
+        return self.tweet_training_usernames
+
+    def get_tweet_training_languages(self):
+        return self.tweet_training_languages
+
+    def get_tweet_training_messages(self):
+        return self.tweet_training_messages
+    
+    def get_tweet_testing_ids(self):
+        return self.tweet_testing_ids
+
+    def get_tweet_testing_usernames(self):
+        return self.tweet_testing_usernames
+
+    def get_tweet_testing_languages(self):
+        return self.tweet_testing_languages
+
+    def get_tweet_testing_messages(self):
+        return self.tweet_testing_messages
