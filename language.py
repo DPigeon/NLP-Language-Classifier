986c02b429b19885bce362632bb9406efefc6327
Kevin Luu <kevinateur@hotmail.com>
2020-03-20 18:05:47 -0400

    [#5] - Add smoothing for initialization

--- language.py
+++ language.py
@@ -3,10 +3,10 @@ from enum import Enum
 """ Method that transforms the enum to dictionary for potential key => value"""
 
 
-def to_dict():
+def to_dict(smoothing=0):
     dict_to_return = {}
     for i in iter(Language):
-        dict_to_return[i] = 0
+        dict_to_return[i] = smoothing   # Initializing
     return dict_to_return
 
 
