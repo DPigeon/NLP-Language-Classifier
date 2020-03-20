942f4faa3059c1d83b25d9adefeb5d6d0d350cd9
Kevin Luu <kevinateur@hotmail.com>
2020-03-20 18:04:53 -0400

    [#5] - Method that initializes a dict of languages

--- language.py
+++ language.py
@@ -1,5 +1,14 @@
 from enum import Enum
 
+""" Method that transforms the enum to dictionary for potential key => value"""
+
+
+def to_dict():
+    dict_to_return = {}
+    for i in iter(Language):
+        dict_to_return[i] = 0
+    return dict_to_return
+
 
 class Language(Enum):
     BASQUE = 'eu'
