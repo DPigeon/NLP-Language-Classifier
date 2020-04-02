3db341ceafced9c851312368b43884f225faa8ad
Kevin Luu <kevinateur@hotmail.com>
2020-04-02 01:27:04 -0400

    [#8] - Add f1 measure, weight, avg

--- output_parser.py
+++ output_parser.py
@@ -21,6 +21,10 @@ def merge_two_dicts(x, y):
     return z
 
 
+def division(n, d):
+    return n / d if d else 0
+
+
 class EvaluationOutputFile:
     entries = []
     accuracy = 0
@@ -30,6 +34,8 @@ class EvaluationOutputFile:
     false_negative_dict = language.to_dict(0)
     false_positive_dict = language.to_dict(0)
     suppose_precision_dict = language.to_dict(0)
+    f1_measure_dict = language.to_dict(0)
+    weighted_values_dict = language.to_dict(0)
 
     def __init__(self, model, v, n, d):
         super().__init__()
@@ -42,23 +48,36 @@ class EvaluationOutputFile:
     def calculate_accuracy(self, number_of_wrong_label):
 
         if number_of_wrong_label is not 0:
-            self.accuracy = number_of_wrong_label / len(self.entries)
+            self.accuracy = 1 - (number_of_wrong_label / len(self.entries))
         else:
             self.accuracy = 1
 
     # e.g. For each english tweet how many were evaluated as english tweet
     def calculate_precision(self):
         total_dict = merge_two_dicts(self.true_positive_dict, self.false_negative_dict)
-        self.precision_dict = {x: float(self.true_positive_dict[x]) / total_dict[x] for x in total_dict}
+        self.precision_dict = {x: division(float(self.true_positive_dict[x]), total_dict[x]) for x in total_dict}
 
     def calculate_recall(self):
         total_dict = merge_two_dicts(self.true_positive_dict, self.false_positive_dict)
         self.recall_dict = {x: float(self.true_positive_dict[x]) / total_dict[x] for x in total_dict}
 
+    # 2PR/P+R
+    def calculate_f1_measure(self):
+        self.f1_measure_dict = {x: division(2 * self.recall_dict[x] * self.precision_dict[x], (self.recall_dict[x] + self.precision_dict[x]))
+                                for x in self.precision_dict}
+        return self.f1_measure_dict
+
+    def calculate_macro_f1(self):
+        return sum(self.f1_measure_dict.values()) / len(self.f1_measure_dict)
+
+    def calculate_weighted_f1(self):
+        calculated_weight = {x: self.f1_measure_dict[x] * self.weighted_values_dict[x] for x in self.f1_measure_dict}
+        return sum(calculated_weight.values()) / sum(self.weighted_values_dict.values())
+
     # What proportion of the instances in the class of interest are labelled correctly?
     # e.g. How many english were actually english
     def add_tp_fn_fp(self, actual, suppose, is_correct):
-
+        self.weighted_values_dict[actual] = self.true_positive_dict[suppose] + 1
         if is_correct == "correct":
             self.true_positive_dict[suppose] = self.true_positive_dict[suppose] + 1
         else:  # Then it's incorrect
@@ -117,7 +136,9 @@ class EvaluationOutputFile:
         self.read_affilated_trace_file()
         if os.path.exists(file_name):
             message = str(self.accuracy) + "\n" + self.convert_lang_dict_to_str(
-                self.precision_dict) + "\n" + self.convert_lang_dict_to_str(self.recall_dict) + "\n"
+                self.precision_dict) + "\n" + self.convert_lang_dict_to_str(self.recall_dict) + "\n" + \
+                      self.convert_lang_dict_to_str(self.calculate_f1_measure()) + "\n" + str(
+                self.calculate_macro_f1()) + "  " + str(self.calculate_weighted_f1())
             file = open(file_name, "a")
             file.write(str(message))
             file.close()
